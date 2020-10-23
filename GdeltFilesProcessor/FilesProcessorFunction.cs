50508ddba844e4011cb9c5cebdc0055c489e0642
M. Ali Iftikhar <ali.iftikhar@outlook.com>
2020-10-23 18:41:02 +0100

    wip - initial entry point of the function

--- GdeltFilesProcessor/FilesProcessorFunction.cs
+++ GdeltFilesProcessor/FilesProcessorFunction.cs
@@ -1,16 +1,39 @@
 using System;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using GdeltFilesProcessor.Core.UseCases.ProcessFile;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
 namespace GdeltFilesProcessor
 {
-    public static class FilesProcessorFunction
+    public class FilesProcessorFunction
     {
-        [FunctionName("FilesProcessorFunction")]
-        public static void Run([QueueTrigger("gdelt-files-for-download", Connection = "AzureWebJobsStorage")]string myQueueItem, ILogger log)
+        private readonly IProcessFileHandler processFileHandler;
+        //private readonly ILogger log;
+
+        public FilesProcessorFunction(IProcessFileHandler processFileHandler)//, ILogger log)
         {
-            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+            this.processFileHandler = processFileHandler;
+            //this.log = log;
+        }
+
+        [FunctionName("FilesProcessorFunction")]
+        public async Task Run([QueueTrigger("gdelt-files-for-download", Connection = "AzureWebJobsStorage")]string myQueueItem)
+        {
+            await ProcessMessage(myQueueItem);
+
+            //this.log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+        }
+
+        private async Task ProcessMessage(string message)
+        {
+            var request = JsonSerializer.Deserialize<ProcessFileRequest>(message);
+
+            await this.processFileHandler.Handle(request);
         }
     }
 }
---
+++ GdeltFilesProcessor/Startup.cs
@@ -0,0 +1,20 @@
+﻿using GdeltFilesProcessor.Core.UseCases.ProcessFile;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+[assembly: FunctionsStartup(typeof(GdeltFilesProcessor.Startup))]
+
+namespace GdeltFilesProcessor
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddTransient<IGdeltCsvEventExtractor, GdeltCsvEventExtractor>();
+            builder.Services.AddTransient<IProcessFileHandler, ProcessFileHandler>();
+        }
+    }
+}
